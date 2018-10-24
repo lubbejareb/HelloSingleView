20dfe9f7eddb51f20172aacb75c4832aedb9795b
lubbe <Ludwig@ludwigs-mbp.lan>
2018-10-24 14:50:27 +0200

    Initial Commit

---
+++ HelloSingleView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloSingleView
+//
+//  Created by Ludwig Jareblad on 2018-10-24.
+//  Copyright © 2018 Ludwig Jareblad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
